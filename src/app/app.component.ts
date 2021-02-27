94df03a46ebe7dbfc10d3f09b4521af7bb02f17e
Gary Saunders <gary@slikinteractive.com>
2021-02-27 13:41:40 -0800

    added basic scaffolding with HTML5 audio play

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'my-site';
+
+  opened: boolean; 
+
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
 }
