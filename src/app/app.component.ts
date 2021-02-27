3b7ae9e210718140995fa9f9897a9a59572e0322
Gary Saunders <gary@slikinteractive.com>
2021-02-27 14:48:45 -0800

    slide toggle now controls monster and audio player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 
 @Component({
@@ -11,8 +11,21 @@ export class AppComponent {
   title = 'my-site';
 
   opened: boolean; 
-
+  showMonster = false;
   color: ThemePalette = 'accent';
   checked = false;
   disabled = false;
+
+  @ViewChild('musicPlayer') musicPlayer: ElementRef;
+
+  ngAfterViewInit() {
+      console.log(this.musicPlayer.nativeElement);
+  }
+
+  changed(){
+    console.log('changedddd................');
+
+    this.checked ?  this.musicPlayer.nativeElement.play() : this.musicPlayer.nativeElement.pause();
+  }
+  
 }
